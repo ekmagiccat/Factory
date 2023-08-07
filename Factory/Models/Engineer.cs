305c5850b201e1cc770ab371473c63d8ee2ff82a
Eva Kemp <eva.j.kemp@gmail.com>
2023-08-06 22:02:51 -0700

    fix typo in footer view

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -6,7 +6,7 @@ namespace Factory.Models
     public class Engineer
     {
         public int EngineerId { get; set; }
-        [Required(ErrorMessage = "The engineer's description can't be empty. Please write a blurb!")]
+        [Required(ErrorMessage = "The engineer's description can't be empty. Please write their name or nickname!")]
         public string Description { get; set; }
         public List<EngineerMachine> JoinEntities { get; }
     }
